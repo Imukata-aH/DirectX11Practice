a351af37b6a58aa3d2dc3a6d1bfa57da5124ad28
Imukata-aH <leahyleahyleahy@gmail.com>
2017-03-07 00:09:18 +0900

    - Add BufferHelper for creating buffer. - Modify Batch class not to create buffers in the class
    TODO: .
    - Batch に渡したバッファが途中で消去されている問題を解決する。DirectX の問題ではなく C++ の使い方の問題。

---
+++ Common/BufferHelper.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+#include "d3dUtil.h"
+
+template <typename T>
+class BufferHelper
+{
+public:
+	static bool CreateVertexBuffer( ID3D11Device** device, const std::vector<T>& vertices, ID3D11Buffer** vertexBuffer )
+	{
+		if ( vertices.size() == 0 )
+		{
+
+			return false;
+		}
+
+		D3D11_BUFFER_DESC vbd;
+
+		vbd.Usage = D3D11_USAGE_IMMUTABLE;
+		vbd.ByteWidth = sizeof( T ) * vertices.size();
+		vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		vbd.CPUAccessFlags = 0;
+		vbd.MiscFlags = 0;
+
+		D3D11_SUBRESOURCE_DATA vinitData;
+		vinitData.pSysMem = &( vertices[0] );
+
+		HR( ( *device )->CreateBuffer( &vbd, &vinitData, vertexBuffer ) );
+
+		return true;
+	}
+
+	static bool CreateIndexBuffer( ID3D11Device** device, const std::vector<T>& indices, ID3D11Buffer** indexBuffer )
+	{
+		if ( indices.size() == 0 )
+		{
+
+			return false;
+		}
+
+		D3D11_BUFFER_DESC ibd;
+
+		ibd.Usage = D3D11_USAGE_IMMUTABLE;
+		ibd.ByteWidth = sizeof( T ) * indices.size();
+		ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		ibd.CPUAccessFlags = 0;
+		ibd.MiscFlags = 0;
+
+		D3D11_SUBRESOURCE_DATA iinitData;
+		iinitData.pSysMem = &( indices[0] );
+
+		HR( ( *device )->CreateBuffer( &ibd, &iinitData, indexBuffer ) );
+
+		return true;
+	}
+};
